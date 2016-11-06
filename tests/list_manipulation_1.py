b1145c9e32bbdb26675aa8ee6c8346d1d62dfff3
Sergei <Sergei@MacBookPro-Sergei.local>
2016-11-06 14:12:03 -0800

    add new funtion

---
+++ tests/list_manipulation_1.py
@@ -0,0 +1,11 @@
+
+spam = ['apples', 'bananas', 'tofu', 'cats']
+
+def list_to_string(a):
+
+    string = ''
+    for item in range((len(a)) - 1):
+         string += a[item] + ',' + ' '
+    return string + "and" + " " + a[(len(a))-1]
+
+print list_to_string(spam)
