9e36203132f260368f5401425417ee058245f8e8
Sergei <Sergei@MacBookPro-Sergei.local>
2016-11-06 18:31:08 -0800

    add grid function

---
+++ tests/picture_grid.py
@@ -0,0 +1,21 @@
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+
+def flip(grid):
+    for row in range(len(grid[0])):
+        string = " "
+        for column in range((len(grid))):
+            string += grid[column][row]
+        print string
+
+    return string
+
+print flip(grid)
