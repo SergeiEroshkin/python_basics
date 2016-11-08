f62a945036b831817b48b9d42433711327f77065
Sergei <Sergei@MacBookPro-Sergei.local>
2016-11-07 20:12:40 -0800

    add dictionary task

---
+++ tests/dictionary_pratice.py
@@ -0,0 +1,18 @@
+# Given Dictionary
+stuff = {'rope': 1,
+        'torch': 6,
+        'gold coin': 42,
+        'dagger': 1,
+        'arrow': 12}
+
+print "Inventory"
+for (value,key) in zip(stuff.values(), stuff.keys()):
+    print value,key
+
+count = 0
+for item in stuff.items(): count += list(item)[1]
+print 'Total number of Items: ' + str(count)
+
+
+
+
