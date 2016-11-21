17c918009137bef2ef681feef95ef11429135c95
Sergei <seregun100@gmail.com>
2016-11-21 09:52:46 -0800

    Update pack-bot.py

--- functions/pack-bot.py
+++ functions/pack-bot.py
@@ -2,9 +2,9 @@ import sys
 import requests
 import json
 
-#suitcase_url = str(sys.argv[1])
+suitcase_url = str(sys.argv[1])
 #'http://pkit.wopr.c2x.io:8000/suitcases/rolly'
-#inventory_url = str(sys.argv[2])
+inventory_url = str(sys.argv[2])
 #'http://pkit.wopr.c2x.io:8000/robots/hey-you/parts'
 
 
@@ -15,35 +15,28 @@ def get_suitcase_size():
 
 def get_density_dict():
     inventory = requests.get('http://pkit.wopr.c2x.io:8000/robots/hey-you/parts').json()
-    print "Initial dic :", len(inventory)
     for part in inventory:
         item_density = ((float(part['value'])/float(part['volume'])))
         part['gross_value'] = item_density
     ccc = sorted(inventory, key=lambda gross_value: gross_value['gross_value'], reverse=True)
     return ccc
-print "Get density :", len(get_density_dict())
 
 
 def get_most_valuable_set():
     suitcase_volume = get_suitcase_size()
-    print suitcase_volume
-    density = get_density_dict()
-    print len(density)
+    density_parts = get_density_dict()
     new_dict ={}
     id_list = []
     total = 0
-    for part in density:
-        max_value = max(density, key=lambda x: x['value'])
-        suitcase_volume -= max_value["volume"]
-        print suitcase_volume, max_value["volume"], part['volume']
-        id_list.append(str(max_value["id"]))
-        density.remove(max_value)
-        total += max_value["volume"]
+    for part in density_parts:
+        suitcase_volume -= part['volume']
         if suitcase_volume <= 0:
             break
+        id_list.append(part['id'])
+        total += part['volume']
 
-    new_dict["part_ids"] = id_list
-    new_dict["value"] = total
+    new_dict['part_ids'] = id_list
+    new_dict['value'] = total
     return json.dumps(new_dict, sort_keys=True, indent=4, separators=(',', ':'))
 
 print get_most_valuable_set()
