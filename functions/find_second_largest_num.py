02930159caa0a52c987c9c6178123356d6320a98
Sergei <Sergei@MacBookPro-Sergei.local>
2016-11-30 22:53:40 -0800

    refactoring

--- functions/find_second_largest_num.py
+++ functions/find_second_largest_num.py
@@ -1,11 +1,9 @@
 num = int(raw_input())
 nums = raw_input()
-nums_list = nums.split(' ')
+nums_list = list(set(nums.split(' ')))
 print nums_list
 n = max(nums_list)
 print n
 l = []
-for i in range(len(nums_list)):
-    if n != nums_list[i]:
-            l.append(nums_list[i])
+l = [nums_list[i] for i in range(len(nums_list)) if n != nums_list[i]]
 print max(l)
