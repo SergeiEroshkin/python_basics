583083e14b2a7b5f0ea3ab8f4ccd1d0df07f4cb6
Sergei <Sergei@MacBookPro-Sergei.local>
2018-01-31 00:00:07 -0800

    Improved logic in collatz_sequence.py

--- functions/collatz_sequence.py
+++ functions/collatz_sequence.py
@@ -1,23 +1,12 @@
 def collatz(number):
-    if number % 2 == 0:
-        new_number = number // 2
-        print new_number
-    else:
-        number % 2 == 13
-        new_number = 3 * number + 1
-        print new_number
-    return new_number
+    while number != 1:
+        if number % 2 == 0:
+            number = number // 2
+            print number
+        else:
+            number = 3 * number + 1
+    return number
 
 
-guess = raw_input('Please enter number: ')
-try:
-    number = int(guess)
-    while collatz(number) != 1:
-        guess = raw_input('Please enter number: ')
-        number = int(guess)
-except:
-    print 'Please enter valid number'
-
-
-
-
+guess = int(raw_input('Please enter number: '))
+print collatz(guess)
