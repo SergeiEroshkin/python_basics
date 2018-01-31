8cc840369e44c6dd877ac39b2c74648febdfebc2
Sergei <Sergei@MacBookPro-Sergei.local>
2018-01-30 23:20:37 -0800

    Fixed issue when invalid average number returns

--- functions/average.py
+++ functions/average.py
@@ -1,10 +1,10 @@
 def average_num(heights):
     """
-
     :param heights: Input string
     :return: Average number in string
     """
-    return sum(set(map(int, heights.split()))) / float(len(set(heights)))
+    return sum(map(int, heights.split())) / float(len(heights.split()))
+
 
 heights = '1 2 3 4 5 6 7 8'
 print average_num(heights)
