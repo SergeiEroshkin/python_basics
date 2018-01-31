ec8bc5b770a6debdf356cac40d390580f2a5787b
Sergei <Sergei@MacBookPro-Sergei.local>
2018-01-31 00:01:20 -0800

    Removed debug print statement

--- functions/collatz_sequence.py
+++ functions/collatz_sequence.py
@@ -2,7 +2,6 @@ def collatz(number):
     while number != 1:
         if number % 2 == 0:
             number = number // 2
-            print number
         else:
             number = 3 * number + 1
     return number
