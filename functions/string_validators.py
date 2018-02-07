c18f2cc3765d7b0078abb2488b3d05472eeb5c2a
Sergei <Sergei@MacBookPro-Sergei.local>
2018-02-06 22:29:14 -0800

    Improved logic in substring_occurance

--- functions/string_validators.py
+++ functions/string_validators.py
@@ -1,22 +1,7 @@
-s = raw_input()
-alpha = False
-alphanum = False
-digits = False
-lowercase = False
-uppercase = False
-for letter in s:
-    if letter.isalnum():
-        alphanum = True
-    if letter.isalpha():
-        alpha = True
-    if letter.isdigit():
-        digits = True
-    if letter.islower():
-        lowercase = True
-    if letter.isupper():
-        uppercase = True
-print alphanum       
-print alpha
-print digits
-print lowercase
-print uppercase
+my_string = raw_input()
+print 'Alphanum: ' + str(my_string.isalnum())
+print 'Alpha: ' + str(my_string.isalpha())
+print 'Digits: ' + str(my_string.isdigit())
+print 'Lowercase: ' + str(my_string.islower())
+print 'Uppercase: ' + str(my_string.isupper())
+
--- functions/substring_occurence.py
+++ functions/substring_occurence.py
@@ -1,8 +1,13 @@
 def get_value(string, substring):
+    return string.count(substring)
+
+
+def get_value_loop(string, substring):
     count = 0
     for letter in range(0, len(string)):
         if string[letter:letter + len(substring)] == substring:
             count += 1
     return count
+
 
 print get_value("lololo lo", "lo")
