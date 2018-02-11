f8ac8f484748c7df729e18bf085da6fa08e26f0b
Sergei <Sergei@MacBookPro-Sergei.local>
2018-02-10 22:05:23 -0800

    Added spam.py

--- functions/spam.py
+++ functions/spam.py
@@ -1,37 +1,48 @@
-"""
-Enter your module docstring with a one-line overview here
-
-and a more detailed description here.
-"""
 SPAM_WORDS = {'opportunity', 'inheritance', 'money', 'rich', 'dictator',
               'discount', 'save', 'free', 'offer', 'credit',
               'loan', 'winner', 'warranty', 'lifetime', 'medicine',
               'claim', 'now', 'urgent', 'expire', 'top',
               'plan', 'prize', 'congratulations', 'help', 'widow'}
 
+
 def spam_indicator(text):
     """
-    Enter your function docstring here
+    This function returns the spam indicator rounded to two decimals
     """
-    # This function returns the spam indicator rounded to two decimals
+    spam_counter = 0
+    unique_words = set(text.lower().split())
+    for word in unique_words:
+        if word in SPAM_WORDS:
+            spam_counter += 1
+        else:
+            continue
+    return round(float(spam_counter) / float(len(unique_words)), 2)
+
 
 def classify(indicator):
     """
-    Enter your function docstring here
+    This function prints the spam classification
     """
-    # This function prints the spam classification
+    spam_threshold = 0.10
+    if indicator > spam_threshold:
+        return 'This message is: SPAM'
+    else:
+        return 'This message is: HAM'
+
 
 def get_input():
     """
-    Enter your function docstring here
+    Get user input and return it
     """
-    # Prompt the user for input and return the input
+    user_input = raw_input('Please enter your message: ')
+    return user_input
+
 
 def main():
-    # Get the user input and save it in a variable
-    # Call spam_indicator to compute the spam indicator and save it
-    # Print the spam_indicator
-    # Call classify to print the classification
+    message = get_input()
+    spam = spam_indicator(message)
+    print 'SPAM indicator: %s' % spam
+    print classify(spam)
 
 
 if __name__ == '__main__':
