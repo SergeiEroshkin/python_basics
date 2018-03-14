a84d223d45ae3768bd2c08b47556269c793928aa
Sergei <Sergei@MacBookPro-Sergei.local>
2018-03-13 20:32:20 -0700

    Infinite sequency problem

---
+++ functions/element_occurence.py
@@ -0,0 +1,24 @@
+# Given infinite sequence : .....-1, -1, 0, 1, 1, 0, -1, -1, 0, 1, 1, 0 ......
+# Write function that will return Nth (case) element in sequence
+# Example num1 = -1; num2 = -1; case = 1 element == 0
+
+my_list = [-1, -1, 0, 1, 1, 0]
+
+
+def get_element(num1, num2, case):
+    a = my_list.index(num1)
+
+
+
+
+
+
+
+
+
+def main():
+    print get_element(-1, -1, 1)
+
+
+if __name__ == '__main__':
+    main()
